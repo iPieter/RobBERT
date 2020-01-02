07494517427582edb1ef1c96176b4381e46e62a4
Thomas Winters <info@thomaswinters.be>
2020-01-02 17:10:09 +0100

    Added extra arguments to the die/dat preprocesor

--- src/preprocess_diedat.py
+++ src/preprocess_diedat.py
@@ -3,7 +3,8 @@ import nltk
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 import argparse
 
-def replace_die_dat(line, flout, fout):
+
+def replace_die_dat_full_sentence(line, flout, fout):
     for i, word in enumerate(nltk.word_tokenize(line)):
         tokens = nltk.word_tokenize(line)
         if word == "die":
@@ -14,33 +15,45 @@ def replace_die_dat(line, flout, fout):
             tokens[i] = "Die"
         elif word == "Die":
             tokens[i] = "dat"
-        
+
         if word.lower() == "die" or word.lower() == "dat":
             choice = random.getrandbits(1)
             results = TreebankWordDetokenizer().detokenize(tokens)
 
             if choice:
-                output = "{} <sep> {}".format(results, line)            
+                output = "{} <sep> {}".format(results, line)
             else:
                 output = "{} <sep> {}".format(line, results)
-                
-            fout.write(output + "\n")
-            flout.write(str(choice) +"\n")   
 
-if __name__ == "__main__":
+            fout.write(output + "\n")
+            flout.write(str(choice) + "\n")
+
+
+def create_parser():
     parser = argparse.ArgumentParser(
         description="Preprocess the europarl corpus for the die-dat task."
     )
-    # TODO Specify your real parameters here.
-    parser.add_argument("path", help="Path to the corpus file.", metavar="path")
+    parser.add_argument("--path", help="Path to the corpus file.", metavar="path", default="data/europarl-v7.nl-en.nl")
+    parser.add_argument("--full_sentence",
+                        help="Storing full sentences separated by a separator, or partial sentences on both sides of the word",
+                        action="store_true", default="true")
+    parser.add_argument("--n", help="Number of lines to take", default="10000")
+
+    return parser
+
+
+if __name__ == "__main__":
+    parser = create_parser()
 
     args = parser.parse_args()
 
-    with open(args.path + '.labels', mode='a') as flout:
-
-        with open(args.path + '.sentences', mode='a') as fout:
-
-            with open(args.path) as fp:
-                for line in fp: 
-                    line = line.replace('\n', '').replace('\r', '')
-                    replace_die_dat(line, flout, fout)
+    if args.full_sentence:
+        with open(args.path + '.labels', mode='a') as labels_output:
+            with open(args.path + '.sentences', mode='a') as sentences_output:
+                with open(args.path) as fp:
+                    for line in fp:
+                        line = line.replace('\n', '').replace('\r', '')
+                        replace_die_dat_full_sentence(line, labels_output, sentences_output)
+    else:
+        # TODO
+        pass
