bd8df1838f614d5deb76898bd1010a7bedeaf32d
Pieter Delobelle <pieterd@cudos01.cs.kuleuven.be>
2020-01-09 10:55:22 +0100

    Parametrized preprocessing for die-dat
    Since BERT, RoBERTa and the transformer versions of those models use
    different separators ([SEP] vs <sep>), this is added to the pre-
    processing script.

--- src/preprocess_diedat.py
+++ src/preprocess_diedat.py
@@ -4,7 +4,7 @@ from nltk.tokenize.treebank import TreebankWordDetokenizer
 import argparse
 
 
-def replace_die_dat_full_sentence(line, flout, fout):
+def replace_die_dat_full_sentence(line, flout, fout, token_format="{} <sep> {}"):
     count = 0
     for i, word in enumerate(nltk.word_tokenize(line)):
         tokens = nltk.word_tokenize(line)
@@ -21,9 +21,9 @@ def replace_die_dat_full_sentence(line, flout, fout):
             results = TreebankWordDetokenizer().detokenize(tokens)
 
             if choice:
-                output = "{} <sep> {}".format(results, line)
+                output = token_format.format(results, line)
             else:
-                output = "{} <sep> {}".format(line, results)
+                output = token_format.format(line, results)
 
             fout.write(output + "\n")
             flout.write(str(choice) + "\n")
@@ -39,6 +39,7 @@ def create_parser():
     parser.add_argument("--path", help="Path to the corpus file.", metavar="path",
                         default="data/raw/europarl-v7.nl-en.nl")
     parser.add_argument("--number", help="Number of examples in the output dataset", type=int, default=10000)
+    parser.add_argument("--format", help="The format of the separators, valid options are `roberta`, `transformers-bert` and `transformers-roberta`.", default="roberta")
 
     return parser
 
@@ -48,13 +49,24 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    # Since we have different token formats for bert, roberta and transformer's roberta, we use an argument.
+    if args.format.lower() == "roberta":
+        token_format = "{} <sep> {}"
+    elif args.format.lower() == "transformers-bert":
+        token_format = "[CLS] {} [SEP] {} [SEP]"
+    elif args.format.lower() == "transformers-roberta":
+        token_format = "<s> {} </s></s> {} </s>" # Yes that second </s> is correct ...
+    else: 
+        print("No valid token format, defaulting to roberta")
+        token_format = "{} <sep> {}"
+
     with open(args.path + '.labels', mode='a') as labels_output:
         with open(args.path + '.sentences', mode='a') as sentences_output:
             with open(args.path) as fp:
                 lines_processed = 0
                 for line in fp:
                     line = line.replace('\n', '').replace('\r', '')
-                    count = replace_die_dat_full_sentence(line, labels_output, sentences_output)
+                    count = replace_die_dat_full_sentence(line, labels_output, sentences_output, token_format)
                     lines_processed += count
                     if lines_processed >= args.number:
                         break
