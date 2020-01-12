0e0b154ddcfdb747fc8944104852e22efd529eb5
Thomas Winters <info@thomaswinters.be>
2020-01-12 16:20:04 +0100

    Set up everything to evaluate multilingual bert and bertje

--- src/bert_masked_lm_adapter.py
+++ src/bert_masked_lm_adapter.py
@@ -5,6 +5,7 @@ import torch
 from transformers import BertForMaskedLM, BertTokenizer
 
 
+
 class BertMaskedLMAdapter:
     def __init__(self, model_name=None, model=None, tokenizer=None):
         self._tokenizer = tokenizer if tokenizer else BertTokenizer.from_pretrained(model_name)
@@ -32,10 +33,25 @@ class BertMaskedLMAdapter:
 
         predicted_indices = list(np.argpartition(predictions[0][0][masked_index], -4)[-topk:])
         predicted_indices.reverse()
-        return [self._tokenizer.convert_ids_to_tokens([predicted_index])[0] for predicted_index in predicted_indices]
+        return [self._convert_token(predicted_index) for predicted_index in predicted_indices]
+
+    def _convert_token(self, predicted_index):
+        text = self._tokenizer.convert_ids_to_tokens([predicted_index])[0]
+        return None, None, text if text.startswith("##") else (" " + text)
 
 
 if __name__ == '__main__':
+    from pathlib import Path
+
+    from src import evaluate_zeroshot_wordlist
     mlm = BertMaskedLMAdapter(model_name='bert-base-multilingual-uncased')
     result = mlm.fill_mask("Er is een meisje <mask> daar loopt.", 1024)
     print(result)
+
+    from src.wordlistfiller import WordListFiller
+
+    wordfiller = WordListFiller(["die", "dat"], model=mlm)
+    print(wordfiller.find_optimal_word("Er is een meisje <mask> daar loopt."))
+    evaluate_zeroshot_wordlist.evaluate(["die", "dat"],
+                                        path=Path("..", "data", "processed", "wordlist","die-dat.test.tsv"),
+                                        odel=mlm, print_step=10)
