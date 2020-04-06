bf994125c27dfe75f7d97007240dec8a759d0725
Pieter Delobelle <pieter.delobelle@gmail.com>
2020-04-06 22:30:09 +0200

    Demo: 'die' vs 'dat' as a rest endpoint with Flask
    As a demo, we release a small Flask example for a rest endpoint
    to analyse sentences. It will return whether a sentence contains
    the correct---according to RobBERT---use of 'die' or 'dat'.

---
+++ examples/die_vs_data_rest_api/app.py
@@ -0,0 +1,4 @@
+from app import create_app
+
+if __name__ == "__main__":
+    create_app().run()
---
+++ examples/die_vs_data_rest_api/app/__init__.py
@@ -0,0 +1,87 @@
+from flask import Flask, request
+import os
+from transformers import RobertaForSequenceClassification, RobertaTokenizer
+import torch
+import nltk
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+import json
+
+def replace_query_token(sentence):
+    "Small utility function to replace a sentence with `_die_` or `_dat_` with the proper RobBERT input."
+    tokens = nltk.word_tokenize(sentence)
+    tokens_swapped = nltk.word_tokenize(sentence)
+    for i, word in enumerate(tokens):
+        if word == "_die_":
+            tokens[i] = "die"
+            tokens_swapped[i] = "dat"
+
+        elif word == "_dat_":
+            tokens[i] = "dat"
+            tokens_swapped[i] = "die"
+
+        elif word == "_Dat_":
+            tokens[i] = "Dat"
+            tokens_swapped[i] = "Die"
+
+        elif word == "_Die_":
+            tokens[i] = "Die"
+
+            tokens_swapped[i] = "Dat"
+
+        if word.lower() == "_die_" or word.lower() == "_dat_":
+            results = TreebankWordDetokenizer().detokenize(tokens)
+            results_swapped = TreebankWordDetokenizer().detokenize(tokens_swapped)
+
+            return "{} <sep> {}".format(results, results_swapped)
+
+    # If we reach the end of the for loop, it means no query token was present
+    raise ValueError("'die' or 'dat' should be surrounded by underscores.")
+
+def create_app(device="cpu"):
+    app = Flask(__name__, instance_relative_config=True)
+
+    print("initializing tokenizer and RobBERT.")
+    tokenizer = RobertaTokenizer.from_pretrained("pdelobelle/robBERT-base")
+
+    robbert = RobertaForSequenceClassification.from_pretrained(
+        '../../../checkpoint-900'
+    )
+
+    print(robbert)
+
+    mask_padding_with_zero = True
+    block_size = 512
+
+    # Disable dropout
+    robbert.eval()
+
+    nltk.download('punkt')
+
+    @app.route('/', methods=["POST"])
+    def hello_world():
+        print(request.form['sentence'])
+        sentence = request.form['sentence']
+        query = replace_query_token(sentence)
+        print(query)
+        tokenized_text = tokenizer.encode(tokenizer.tokenize(query)[- block_size + 3 : -1])
+
+        input_mask = [1 if mask_padding_with_zero else 0] * len(tokenized_text)
+
+        pad_token = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
+        while len(tokenized_text) < block_size:
+            tokenized_text.append(pad_token)
+            input_mask.append(0 if mask_padding_with_zero else 1)
+            #segment_ids.append(pad_token_segment_id)
+            #p_mask.append(1)
+
+        #self.examples.append([tokenizer.build_inputs_with_special_tokens(tokenized_text[0 : block_size]), [0], [0]])
+        batch = tuple(torch.tensor(t).to(torch.device(device)) for t in [tokenized_text[0 : block_size - 3], input_mask[0 : block_size- 3], [0], [1] [0]])
+        inputs = {"input_ids": batch[0].unsqueeze(0), "attention_mask": batch[1].unsqueeze(0), "labels": batch[3].unsqueeze(0)}
+        with torch.no_grad():
+            outputs = robbert(**inputs)
+
+            rating = int(outputs[1].argmax().numpy())
+            confidence = float(outputs[1][0,rating].numpy())
+            return json.dumps({"rating": rating, "interpretation": "incorrect" if rating == 1 else "correct", "confidence": confidence, "sentence": sentence })
+
+    return app
