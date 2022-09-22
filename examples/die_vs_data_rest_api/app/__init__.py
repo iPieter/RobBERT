67b7658823480a23fe3c03b0d67c224b29dab519
Pieter Delobelle <pieter.delobelle@gmail.com>
2022-09-22 17:03:56 +0200

    New api endpoint for demo

--- examples/die_vs_data_rest_api/app/__init__.py
+++ examples/die_vs_data_rest_api/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
 import os
-from transformers import RobertaForSequenceClassification, RobertaForMaskedLM, RobertaTokenizer
+from transformers import (
+    RobertaForSequenceClassification,
+    RobertaForMaskedLM,
+    RobertaTokenizer,
+)
 import torch
 import nltk
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 import json
+import re
 
 
 def replace_query_token(sentence):
@@ -39,7 +44,7 @@ def replace_query_token(sentence):
     raise ValueError("'die' or 'dat' should be surrounded by underscores.")
 
 
-def create_app(model_path: str, fast_model_path:str, device="cpu"):
+def create_app(model_path: str, fast_model_path: str, device="cpu"):
     """
     Create the flask app.
 
@@ -51,19 +56,27 @@ def create_app(model_path: str, fast_model_path:str, device="cpu"):
 
     print("initializing tokenizer and RobBERT.")
     if model_path:
-        tokenizer: RobertaTokenizer = RobertaTokenizer.from_pretrained(model_path, use_auth_token=True)
-        robbert = RobertaForSequenceClassification.from_pretrained(model_path, use_auth_token=True)
+        tokenizer: RobertaTokenizer = RobertaTokenizer.from_pretrained(
+            model_path, use_auth_token=True
+        )
+        robbert = RobertaForSequenceClassification.from_pretrained(
+            model_path, use_auth_token=True
+        )
         robbert.eval()
         print("Loaded finetuned model")
 
     if fast_model_path:
-        fast_tokenizer: RobertaTokenizer = RobertaTokenizer.from_pretrained(fast_model_path, use_auth_token=True)
-        fast_robbert = RobertaForMaskedLM.from_pretrained(fast_model_path, use_auth_token=True)
+        fast_tokenizer: RobertaTokenizer = RobertaTokenizer.from_pretrained(
+            fast_model_path, use_auth_token=True
+        )
+        fast_robbert = RobertaForMaskedLM.from_pretrained(
+            fast_model_path, use_auth_token=True
+        )
         fast_robbert.eval()
 
         print("Loaded MLM model")
 
-        possible_tokens = ['die', 'dat', 'Die', 'Dat']
+        possible_tokens = ["die", "dat", "Die", "Dat"]
 
         ids = fast_tokenizer.convert_tokens_to_ids(possible_tokens)
 
@@ -72,20 +85,78 @@ def create_app(model_path: str, fast_model_path:str, device="cpu"):
 
     # Disable dropout
 
-    nltk.download('punkt')
-    
+    nltk.download("punkt")
+
     if fast_model_path:
-        @app.route('/fast', methods=["POST"])
+
+        @app.route("/disambiguation/mlm/all", methods=["POST"])
+        def split():
+            sentence = request.form["sentence"]
+
+            response = []
+            old_pos = 0
+            for match in re.finditer(r"(die|dat|Die|Dat)+", sentence):
+                print(
+                    "match",
+                    match.group(),
+                    "start index",
+                    match.start(),
+                    "End index",
+                    match.end(),
+                )
+                with torch.no_grad():
+                    query = (
+                        sentence[: match.start()] + "<mask>" + sentence[match.end() :]
+                    )
+                    print(query)
+                    if match.start() > 0:
+                        response.append({"part": sentence[old_pos: match.start()]})
+
+                    old_pos = match.end()
+                    inputs = fast_tokenizer.encode_plus(query, return_tensors="pt")
+
+                    outputs = fast_robbert(**inputs)
+                    masked_position = torch.where(
+                        inputs["input_ids"] == fast_tokenizer.mask_token_id
+                    )[1]
+                    if len(masked_position) > 1:
+                        return "No two queries allowed in one sentence.", 400
+
+                    print(outputs.logits[0, masked_position, ids])
+                    token = outputs.logits[0, masked_position, ids].argmax()
+
+                    confidence = float(outputs.logits[0, masked_position, ids].max())
+
+                    response.append({
+                        "predicted": possible_tokens[token],
+                        "input": match.group(),
+                        "interpretation": "correct"
+                        if possible_tokens[token] == match.group()
+                        else "incorrect",
+                        "confidence": confidence,
+                        "sentence": sentence,
+                    })
+
+                    # This would be a good place for logging/storing queries + results
+                    print(response)
+
+            # inputs = fast_tokenizer.encode_plus(query, return_tensors="pt")
+            response.append({"part": sentence[match.end():]})
+            return json.dumps(response)
+
+        @app.route("/disambiguation/mlm", methods=["POST"])
         def fast():
-            sentence = request.form['sentence']
+            sentence = request.form["sentence"]
             for i, x in enumerate(possible_tokens):
                 if f"_{x}_" in sentence:
                     masked_id = i
-                    query = sentence.replace(f"_{x}_" , fast_tokenizer.mask_token)
+                    query = sentence.replace(f"_{x}_", fast_tokenizer.mask_token)
 
             inputs = fast_tokenizer.encode_plus(query, return_tensors="pt")
 
-            masked_position = torch.where(inputs['input_ids'] == fast_tokenizer.mask_token_id)[1]
+            masked_position = torch.where(
+                inputs["input_ids"] == fast_tokenizer.mask_token_id
+            )[1]
             if len(masked_position) > 1:
                 return "No two queries allowed in one sentence.", 400
 
@@ -93,27 +164,33 @@ def create_app(model_path: str, fast_model_path:str, device="cpu"):
             with torch.no_grad():
                 outputs = fast_robbert(**inputs)
 
-                print(outputs.logits[0,masked_position,ids] )
-                token = outputs.logits[0,masked_position,ids].argmax()
-                
-                confidence = float(outputs.logits[0,masked_position,ids].max())
+                print(outputs.logits[0, masked_position, ids])
+                token = outputs.logits[0, masked_position, ids].argmax()
 
-                response = {"rating": possible_tokens[token], "interpretation": "correct" if token == masked_id else "incorrect",
-                            "confidence": confidence, "sentence": sentence}
+                confidence = float(outputs.logits[0, masked_position, ids].max())
+
+                response = {
+                    "rating": possible_tokens[token],
+                    "interpretation": "correct" if token == masked_id else "incorrect",
+                    "confidence": confidence,
+                    "sentence": sentence,
+                }
 
                 # This would be a good place for logging/storing queries + results
                 print(response)
 
                 return json.dumps(response)
 
-    
     if model_path:
-        @app.route('/', methods=["POST"])
+
+        @app.route("/disambiguation/classifier", methods=["POST"])
         def main():
-            sentence = request.form['sentence']
+            sentence = request.form["sentence"]
             query = replace_query_token(sentence)
 
-            tokenized_text = tokenizer.encode(tokenizer.tokenize(query)[- block_size + 3: -1])
+            tokenized_text = tokenizer.encode(
+                tokenizer.tokenize(query)[-block_size + 3 : -1]
+            )
 
             input_mask = [1 if mask_padding_with_zero else 0] * len(tokenized_text)
 
@@ -125,18 +202,32 @@ def create_app(model_path: str, fast_model_path:str, device="cpu"):
                 # p_mask.append(1)
 
             # self.examples.append([tokenizer.build_inputs_with_special_tokens(tokenized_text[0 : block_size]), [0], [0]])
-            batch = tuple(torch.tensor(t).to(torch.device(device)) for t in
-                        [tokenized_text[0: block_size - 3], input_mask[0: block_size - 3], [0], [1][0]])
-            inputs = {"input_ids": batch[0].unsqueeze(0), "attention_mask": batch[1].unsqueeze(0),
-                    "labels": batch[3].unsqueeze(0)}
+            batch = tuple(
+                torch.tensor(t).to(torch.device(device))
+                for t in [
+                    tokenized_text[0 : block_size - 3],
+                    input_mask[0 : block_size - 3],
+                    [0],
+                    [1][0],
+                ]
+            )
+            inputs = {
+                "input_ids": batch[0].unsqueeze(0),
+                "attention_mask": batch[1].unsqueeze(0),
+                "labels": batch[3].unsqueeze(0),
+            }
             with torch.no_grad():
                 outputs = robbert(**inputs)
 
                 rating = outputs[1].argmax().item()
                 confidence = outputs[1][0, rating].item()
 
-                response = {"rating": rating, "interpretation": "incorrect" if rating == 1 else "correct",
-                            "confidence": confidence, "sentence": sentence}
+                response = {
+                    "rating": rating,
+                    "interpretation": "incorrect" if rating == 1 else "correct",
+                    "confidence": confidence,
+                    "sentence": sentence,
+                }
 
                 # This would be a good place for logging/storing queries + results
                 print(response)
